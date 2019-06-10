752be812d629fc998d65dcd416764199b67ce2cb
AndrewOmajuwa <omajuwaandrew@gmail.com>
2019-06-10 19:28:06 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Hello World</h2>
+          <h2>Welcome to Codaisseur</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Lets get started by intoducing ourselves, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
